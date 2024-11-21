343fd184280593ea19a38fd83eb8327325033127
Benjamin Navarrete <bnavarretem@uft.edu>
2024-11-20 21:29:35 -0300

    Delete MixMaster/MixMaster BackEnd/api 1 directory

--- MixMaster/MixMaster BackEnd/api 1/src/main/java/com/baresflix/api/exceptions/JwtResponseException.java
+++
@@ -1,8 +0,0 @@
-package com.baresflix.api.exceptions;
-
-
-public class JwtResponseException extends Exception {
-    public JwtResponseException(String message) {
-        super(message);
-    }
-}
